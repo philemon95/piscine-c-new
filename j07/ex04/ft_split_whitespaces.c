421e6af9ce32b62316ccfcf7efae1e2045aa1b98
Philemon PERROT <phperrot@e1r5p1.42.fr>
2018-07-14 14:42:21 +0200

    s

--- j07/ex04/ft_split_whitespaces.c
+++ j07/ex04/ft_split_whitespaces.c
@@ -6,7 +6,7 @@
 /*   By: phperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/14 11:16:49 by phperrot          #+#    #+#             */
-/*   Updated: 2018/07/14 13:09:33 by phperrot         ###   ########.fr       */
+/*   Updated: 2018/07/14 14:42:12 by phperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int			length(char *str)
 	return (i);
 }
 
-char **ft_split_whitespaces(char *str)
+char	**ft_split_whitespaces(char *str)
 {
 	int i;
 	int j;
@@ -34,22 +34,20 @@ char **ft_split_whitespaces(char *str)
 	int nb_char;
 	char **tabwords;
 	int index;
+
 	i = 0;
 	nb_words = 1;
-	while ((str[i] == ' ') | (str[i] == '\n') | (str[i] == '\r') | 
+	while ((str[i] == ' ') | (str[i] == '\n') | (str[i] == '\r') |
 			(str[i] == '\t') | (str[i] == '\v'))
 		i++;
-	
 	j = i;
-	/* counting numbers of separators in the remaining part of the string */
-	while (str[i+1] != '\0')
+	while (str[i + 1] != '\0')
 	{
-		if (((str[i] == ' ') | (str[i] == '\n') | (str[i] == '\r') | (str[i] == '\t') | (str[i] == '\v')) &&(!((str[i+1] == ' ') | (str[i+1] == '\n') | (str[i+1] == '\r') | (str[i+1] == '\t') | (str[i+1] == '\v'))))
-		nb_words++;	
+		if (((str[i] == ' ') | (str[i] == '\n') | (str[i] == '\r') | (str[i] == '\t') | (str[i] == '\v')) && (!((str[i + 1] == ' ') | (str[i + 1] == '\n') | (str[i + 1] == '\r') | (str[i + 1] == '\t') | (str[i + 1] == '\v'))))
+			nb_words++;
 		i++;
 	}
-	
-	tabwords = malloc(sizeof(*str) * (nb_words + 1));
+	tabwords = (char **)malloc(sizeof(char*) * (nb_words + 1));
 	i = 0;
 	while (i < nb_words)
 	{
@@ -57,48 +55,42 @@ char **ft_split_whitespaces(char *str)
 		while (((str[j] == ' ') | (str[j] == '\n') | (str[j] == '\r') |
 					(str[j] == '\t') | (str[j] == '\v') | (str[j] == '\0')))
 			j++;
-		
 		index = j;
-
 		while (!((str[j] == ' ') | (str[j] == '\n') | (str[j] == '\r') |
 				(str[j] == '\t') | (str[j] == '\v') | (str[j] == '\0')))
 		{
 			nb_char++;
-			j++;;
+			j++;
 		}
-
-		tabwords[i] = malloc(sizeof(*str) * (nb_char + 1));
-		k = 0;	
-			while (k < nb_char)
-			{
-				tabwords[i][k] = str[k+index];
-				printf("%c", str[k+index]);
-				k++;
-			}
+		tabwords[i] = (char*)malloc(sizeof(char) * (nb_char + 1));
+		k = 0;
+		while (k < nb_char)
+		{
+			tabwords[i][k] = str[k + index];
+			printf("%c", str[k + index]);
+			k++;
+		}
 		printf(" ");
 		i++;
-		j++; /* pour decaler et squuzer un separateur */
+		j++;
 	}
 	printf("\n ----- %d ----\n", nb_words);
-	return(tabwords);
+	tabwords[nb_words] = 0;
+	return (tabwords);
 }
 
-
-int		main(void)
+int			main(void)
 {
-	char *str;
-	char **res;
-	int i;
+	char	*str;
+	char	**res;
+	int		i;
+	int		nb;
 
-	str="      ekwpeddekop  \n    ek dwk dewkodke ee s    a ";
-	res = ft_split_whitespaces(str);	
-	i = 0;
-	while (res[0][i] != '\0')
-	{
-		printf("%c", res[0][i]);
-		i++;
-	}
-
-	
-	return(0);
+	nb = 6;
+	str = "ekwpeddekop  \n    ek dwk dewkodke ee sa";
+	res = ft_split_whitespaces(str);
+	i = 8;
+	printf("%s", res[nb]);
+	i++;
+	return (0);
 }
